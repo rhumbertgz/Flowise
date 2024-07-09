c3947b68e351a1c970c15c9785b8616279c112db
Rodriguez Avila Humberto <humberto.rodriguez_avila@nokia-bell-labs.com>
2024-07-09 16:17:35 +0200

    Feature - Add search keyboard shortcut hook

---
+++ packages/ui/src/hooks/useSearhShortcut.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+import { isMac } from '@/utils/platformHelper'
+
+const useSearchShorcut = (inputRef) => {
+    useEffect(() => {
+        const component = inputRef.current
+        const handleKeyDown = (event) => {
+            if ((isMac && event.metaKey && event.key === 'f') || (!isMac && event.ctrlKey && event.key === 'f')) {
+                event.preventDefault()
+                component.focus()
+            }
+        }
+
+        const handleInputEscape = (event) => {
+            if (event.key === 'Escape') component.blur()
+        }
+
+        inputRef.current.addEventListener('keydown', handleInputEscape)
+        document.addEventListener('keydown', handleKeyDown)
+
+        return () => {
+            component.addEventListener('keydown', handleInputEscape)
+            document.removeEventListener('keydown', handleKeyDown)
+        }
+    })
+}
+
+export default useSearchShorcut
